6a922c7aff47f14bbd8e6e16b920939a9e23e1c9
SHREE BALAJI <shreebalajisiva19@gmail.com>
2023-08-14 22:59:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 let nav = document.querySelector("nav");
 let scrollBtn = document.querySelector(".scroll-button a");
 console.log(scrollBtn);
-let val;
+let   val;
 window.onscroll = function() {
   if(document.documentElement.scrollTop > 20){
     nav.classList.add("sticky");
